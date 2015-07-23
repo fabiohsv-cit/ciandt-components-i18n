0d97aae2c35ac9603ffb14c3dbe5e21945dc7027
Fábio Henrique da Silva Viana <fabiohsv@ciandt.com>
2015-07-23 15:57:42 -0300

    load correct resorces and $locale

--- i18n.js
+++ i18n.js
@@ -1,80 +1,257 @@
 ﻿"use strict";
 
-define(['angular'], function (app) {
-    angular.module("ciandt.components.i18n", []).factory("ciandt.components.i18n.Localize", ["$http", "$rootScope", "$window",
-        function ($http, $rootScope, $window) {
-            var localize;
-            return localize = {
-                language: "pt",
-                url: void 0,
-                resourceFileLoaded: !1,
-                successCallback: function (data) {
-                    return localize.dictionary = data, localize.resourceFileLoaded = !0, $rootScope.$broadcast("localizeResourcesUpdated");
-                },
-                setLanguage: function (value) {
-                    return localize.language = value.toLowerCase().split("-")[0], localize.initLocalizedResources();
-                },
-                setUrl: function (value) {
-                    return localize.url = value, localize.initLocalizedResources();
-                },
-                buildUrl: function () {
-                    return localize.language || (localize.language = ($window.navigator.userLanguage || $window.navigator.language).toLowerCase(), localize.language = localize.language.split("-")[0]), "app/common/i18n/resources_" + localize.language + ".json";
-                },
-                initLocalizedResources: function () {
-                    var url;
-                    return url = localize.url || localize.buildUrl(), $http({
-                        method: "GET",
-                        url: url,
-                        cache: !1
-                    }).success(localize.successCallback).error(function () {
-                        return $rootScope.$broadcast("localizeResourcesUpdated");
-                    });
-                },
-                get: function (value) {
-                    var result, valueLowerCase;
-                    result = (result = void 0, localize.dictionary && value ? (valueLowerCase = value.toLowerCase(), result = "" === localize.dictionary[valueLowerCase] ? value : localize.dictionary[valueLowerCase]) : result = value, result);
-                    if (result) {
-                        return result;
-                    } else {
-                        return value;
-                    }
-                }
-            };
-        }]).directive("i18n", ["ciandt.components.i18n.Localize", function (localize) {
-            var i18nDirective;
-            return i18nDirective = {
-                restrict: "EA",
-                updateText: function (ele, input, placeholder, title) {
-                    var result;
-                    if (title) {
-                        result = localize.get(placeholder);
-                        ele.attr("title", result);
-                    }
-                    return result = void 0, "i18n-placeholder" === input ? (result = localize.get(placeholder), ele.attr("placeholder", result)) : input.length >= 1 ? (result = localize.get(input), ele.text(result)) : void 0;
-                },
-                link: function (scope, ele, attrs) {
-                    if (typeof attrs.i18n == 'string' && attrs.title) {
-                        // TODO Viana: acertar trecho quando i18n é usado para transformar um title
-                        return;
-                    }
+define(['angular', 'angular-dynamic-locale'], function () {
+    angular.module("ciandt.components.i18n", ['ciandt.components.utilities', 'tmh.dynamicLocale']).constant('ciandt.components.i18n.LocalizeConfig', {
+        defaultLanguage: 'pt',
+        supportedLanguage: ['pt', 'en'],
+        localePath: 'assets/libs/angular-i18n/angular-locale_{{locale}}.js'
+    }).factory("ciandt.components.i18n.Localize", ['ciandt.components.utilities.Utilities', '$rootScope', 'ciandt.components.i18n.LocalizeConfig', '$log', 'tmhDynamicLocale', function (utilities, $rootScope, LocalizeConfig, $log, tmhDynamicLocale) {
+        var userLang;
+        try {
+            userLang = JSON.parse(localStorage.getItem('i18n_lang'));
+        } catch (e) { }
 
-                    if (ele.is('i18n')) {
-                        i18nDirective.updateText(ele, ele.text());
-                    }
-                    var observe = 'i18n';
-                    if (attrs.i18n == '' && attrs.title) {
-                        observe = 'title';
-                    }
-                    return scope.$on("localizeResourcesUpdated", function () {
-                        var key = attrs.i18n ? attrs.i18n : ele.text();
-                        return i18nDirective.updateText(ele, key, attrs.placeholder, attrs.title);
-                    }), attrs.$observe(observe, function (value) {
-                        if (!value) {
-                            value = ele.text();
-                        }
-                        return i18nDirective.updateText(ele, value, attrs.placeholder, attrs.title);
-                    });
+        if (!userLang) {
+            userLang = navigator.language || navigator.userLanguage;
+        }
+
+        var language = LocalizeConfig.defaultLanguage;
+        if (userLang) {
+            language = userLang.toLowerCase().split("-")[0];
+            // se linguagem não for suportada, considera default
+            if (!_.find(LocalizeConfig.supportedLanguage, function (item) { return item == language; })) {
+                language = LocalizeConfig.defaultLanguage;
+            }
+        }
+
+        $log.info('Lang setted to: ' + language);
+
+        var resources = [];
+        var dictionary = {};
+        var regexDictionary = {};
+
+        var onfinishLoadResources = function (lang) {
+            $rootScope.$broadcast("localizeResourcesUpdated");
+        };
+
+        var onfinishChangeLanguage = function (lang, onfinish) {
+            var event;
+            event = $rootScope.$on('$localeChangeSuccess', function (evt, lang) {
+                $log.debug('ngLocale_' + lang + ' loaded');
+
+                // atribui lang escolhida
+                if (language != lang) {
+                    language = lang;
+                    $log.debug('Language changed to: ' + lang);
                 }
-            };
-        }]);
+
+                // atualiza cache com lang escolhida
+                utilities.setLocalStorage('i18n_lang', lang);
+
+                if (moment) {
+                    // atribui ao moment a linguagem corrente
+                    moment.locale(lang);
+                }
+
+                if (onfinish) {
+                    onfinish(lang);
+                }
+                // remove listener
+                if (event) {
+                    event();
+                }
+            });
+
+            tmhDynamicLocale.set(lang);
+        };
+
+        var changeLanguage = function (lang) {
+            if (language != lang) {
+                loadResources(lang, resources, function (lang) {
+                    onfinishChangeLanguage(lang, function () {
+                        onfinishLoadResources(lang);
+                    });
+                });
+            } else {
+                onfinishChangeLanguage(lang);
+            }
+        };
+
+        changeLanguage(language);
+
+        var loadResources = function (lang, res, onfinish) {
+            if (lang != LocalizeConfig.defaultLanguage) {
+                var _resources = [];
+                angular.forEach(res, function (resource) {
+                    _resources.push('json!' + resource.replace(/{lang}/g, lang));
+                });
+
+                require(_resources, function () {
+                    $log.debug('Resources loaded:');
+                    $log.debug(_resources);
+                    // se linguagem foi alterada, reseta dictionaries
+                    if (lang != language) {
+                        dictionary = {};
+                        regexDictionary = {};
+                    }
+                    // pra cada resources retornado adiciona ao dictionary
+                    angular.forEach(arguments, function (data) {
+                        if (data) {
+                            if (data.resources || data.regexResources) {
+                                if (data.resources) {
+                                    dictionary = angular.extend(dictionary, data.resources);
+                                }
+                                if (data.regexResources) {
+                                    regexDictionary = angular.extend(regexDictionary, data.regexResources);
+                                }
+                            } else {
+                                dictionary = angular.extend(dictionary, data);
+                            }
+                        }
+                    });
+                    onfinish(lang);
+                });
+            } else {
+                dictionary = {};
+                regexDictionary = {};
+                onfinish(lang);
+            }
+        };
+
+        return {
+            addResource: function (resource) {
+                resources.push(resource);
+                // se linguagem atual for diferente da default, carrega resource
+                if (language != LocalizeConfig.defaultLanguage) {
+                    loadResources(language, [resource], onfinishLoadResources);
+                }
+            },
+
+            setLanguage: function (value) {
+                value = value.toLowerCase().split("-")[0];
+                // se linguagem atual for diferente da linguagem informada, carrega resources
+                if (language != value) {
+                    changeLanguage(value);
+                }
+            },
+
+            getLanguage: function () {
+                return language;
+            },
+
+            get: function (value) {
+                // se tem value e linguagem for diferente da default, então traduz
+                // se for linguagem default nao precisa traduzir
+                if (value && language != LocalizeConfig.defaultLanguage) {
+                    if (dictionary && dictionary[value.toLowerCase()]) {
+                        return dictionary[value.toLowerCase()];
+                    } else
+                        if (regexDictionary) {
+                            // busca usando regex
+                            // value: Deseja excluir o modulo Controle de Acesso?
+                            // regex: Deseja excluir o modulo ([^(?)]+)\?
+                            // tradução: Would you like to remove the module $1?
+                            // estratégia: loop em todas as chaves fazendo match com o value, o que der match retorna fazendo replace dos matches encontrados
+                            for (var key in regexDictionary) {
+                                var reg = new RegExp(key, 'gi');
+                                var m = value.match(reg);
+                                if (m && m.length > 0) {
+                                    var translate = regexDictionary[key];
+                                    return value.replace(reg, translate);
+                                }
+                            }
+                        }
+                }
+                return value;
+            }
+        }
+    }]).directive("i18n", ["ciandt.components.i18n.Localize", function (localize) {
+        var i18nDirective;
+        return i18nDirective = {
+            restrict: "EA",
+            update: function (ele, attrs) {
+                var i18n;
+
+                // altera body do elemento caso não sejam tags html, se for apenas texto
+                if (ele.html().trim() == ele.text().trim()) {
+                    var text = ele.text().trim();
+                    if (attrs.i18n) {
+                        ele.text(localize.get(attrs.i18n));
+                    } else if (text) {
+                        i18n = text;
+                        ele.text(localize.get(i18n));
+                    }
+                }
+
+                // altera placeholder
+                if (attrs.placeholder) {
+                    if (attrs.i18n) {
+                        ele.attr("placeholder", localize.get(attrs.i18n));
+                    } else {
+                        i18n = attrs.placeholder;
+                        ele.attr("placeholder", localize.get(attrs.placeholder));
+                    }
+                }
+
+                // altera title
+                if (attrs.title) {
+                    if (attrs.i18n) {
+                        ele.attr("title", localize.get(attrs.i18n));
+                    } else {
+                        i18n = attrs.title;
+                        ele.attr("title", localize.get(attrs.title));
+                    }
+                }
+
+                // altera alt
+                if (attrs.alt) {
+                    if (attrs.i18n) {
+                        ele.attr("alt", localize.get(attrs.i18n));
+                    } else {
+                        i18n = attrs.alt;
+                        ele.attr("alt", localize.get(attrs.alt));
+                    }
+                }
+
+                if (i18n) {
+                    ele.attr("i18n", i18n);
+                }
+            },
+            link: function (scope, ele, attrs) {
+                var observe = function (value) {
+                    i18nDirective.update(ele, attrs);
+                };
+
+                if (ele.html().trim() == ele.text().trim()) {
+                    i18nDirective.update(ele, attrs);
+                }
+
+                if (attrs.title) {
+                    attrs.$observe('title', observe);
+                }
+
+                if (attrs.alt) {
+                    attrs.$observe('alt', observe);
+                }
+
+                if (attrs.placeholder) {
+                    attrs.$observe('placeholder', observe);
+                }
+
+                if (attrs.i18n) {
+                    attrs.$observe('i18n', observe);
+                }
+
+                scope.$on("localizeResourcesUpdated", function () {
+                    i18nDirective.update(ele, attrs);
+                });
+            }
+        };
+    }]).filter('i18n', ['ciandt.components.i18n.Localize', function (localize) {
+        return function (value) {
+            return localize.get(value);
+        }
+    }]).config(['tmhDynamicLocaleProvider', 'ciandt.components.i18n.LocalizeConfig', function (tmhDynamicLocaleProvider, LocalizeConfig) {
+        tmhDynamicLocaleProvider.localeLocationPattern(LocalizeConfig.localePath);
+    }]);
 });
